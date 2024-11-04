67e85f80d7fe646d6d774db0bce1a581698659e8
cjdelacruz <cjdelacruz0521@gmail.com>
2024-11-04 18:55:17 +0700

    Initial commit with colorPreferences.js

---
+++ colorPreference.js
@@ -0,0 +1,11 @@
+// Create an array to store colors
+let colors = ["white", "green", "black"];
+
+// Print the initial array
+console.log("Your favorite colors are:", colors);
+
+// Add a new color to the array
+colors.push("brown");
+
+// Print the updated array
+console.log("Updated favorite colors:", colors);
